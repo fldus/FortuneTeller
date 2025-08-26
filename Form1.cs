aa4b5f3888bbe4000e7db79c9e531d59dee5e273
Kim Riyeon <kry59965@gmail.com>
2025-08-26 15:05:15 +0900

    #1 내역 불러오기, 포츈텔러 정보 폼 생성 및 호출을 위한 메뉴 구현

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,22 @@ namespace FortuneTeller
         {
             InitializeComponent();
         }
+
+        private void 내역불러오기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormHistory form = new FormHistory();
+            form.Show();
+        }
+
+        private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void 포츈텔러정보ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormAbout form = new FormAbout();
+            form.ShowDialog();
+        }
     }
 }
